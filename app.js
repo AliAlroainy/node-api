d35d65b5c76026ee68e1833658fe964905860fe0
Ali <aliabduh.2020@gmail.com>
2022-02-19 00:23:47 +0300

    run the server

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
  const express = require('express');
- const app = express();
+ const { render } = require('express/lib/response');
+const app = express();
 
- 
+app.set('view engine','ejs');
+
+app.use(express.static('public'));
+const port = process.env.PORT || 3000 ;
+
+app.get("/", (req,res)=>{
+    res.render("index");
+    res.end();
+});
+
+
+app.listen(port);
+console.log('server started');
